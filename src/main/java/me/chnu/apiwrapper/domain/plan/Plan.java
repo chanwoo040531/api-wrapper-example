d7de54daec8894d48a9e413a8ed99184625f5e77
chanwoo040531 <chanwoo040531@gmail.com>
2024-01-13 23:11:15 +0900

    [feat] create plans table

---
+++ src/main/java/me/chnu/apiwrapper/domain/plan/Plan.java
@@ -0,0 +1,34 @@
+package me.chnu.apiwrapper.domain.plan;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.chnu.apiwrapper.domain.BaseEntity;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "plans")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Plan extends BaseEntity {
+    private String name;
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime lastUpdatedAt;
+
+    @Builder
+    private Plan(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
---
+++ src/main/java/me/chnu/apiwrapper/domain/plan/PlanRepository.java
@@ -0,0 +1,6 @@
+package me.chnu.apiwrapper.domain.plan;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlanRepository extends JpaRepository<Plan, Long> {
+}
